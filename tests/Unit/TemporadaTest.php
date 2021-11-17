009467b78f5641086ec4d924e2bda68e57adf564
Cláudio Oliveira <83782979+claudio1994-oliveira@users.noreply.github.com>
2021-11-17 19:23:14 -0300

    Add files via upload

---
+++ tests/Unit/TemporadaTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Episodio;
+use App\Models\Temporada;
+use PHPUnit\Framework\TestCase;
+
+class TemporadaTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    /** @var Temporada */
+    private $temporada;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $temporada = new Temporada();
+        $episodio1 = new Episodio();
+        $episodio1->assistido = true;
+        $episodio2 = new Episodio();
+        $episodio2->assistido = false;
+        $episodio3 = new Episodio();
+        $episodio3->assistido = true;
+        $temporada->episodios->add($episodio1);
+        $temporada->episodios->add($episodio2);
+        $temporada->episodios->add($episodio3);
+
+        $this->temporada = $temporada;
+    }
+
+    public function testBuscaPorEpisodiosAssistidos()
+    {
+        $episodiosAssistidos = $this->temporada->getEpisodiosAssistidos();
+        $this->assertCount(2, $episodiosAssistidos);
+        foreach ($episodiosAssistidos as $episodio) {
+            $this->assertTrue($episodio->assistido);
+        }
+    }
+
+    public function testBuscaTodosOsEpisodios()
+    {
+        $episodios = $this->temporada->episodios;
+        $this->assertCount(3, $episodios);
+    }
+}
